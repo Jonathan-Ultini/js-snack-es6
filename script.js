5d6c5a693fbbf24f3dacbbfc2af18fb5ded84a88
Jonathan-Ultini <jonathanultini@gmail.com>
2024-08-29 13:38:00 +0200

    snack 1

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('OK')
+// SNACK 1
+//lista invitati
+const tableName = "Tavolo Vip";
+const guests = [
+  'Brad Pitt',
+  'Johnny Depp',
+  'Lady Gaga',
+  'Cristiano Ronaldo',
+  'Georgina Rodriguez',
+  'Chiara Ferragni',
+  'George Clooney',
+  'Amal Clooney',
+  'Fedez',
+  'Amadeus',
+  'Fiorello'
+];
+
+//trasformo ogni elemento array in oggetto
+const placeholders = guests.map((guest, index) => ({
+  tableName: tableName,
+  guestName: guest,
+  place: index + 1
+}));
+
+console.log(placeholders);
